51c3efd25425d54ef8279adc10e9e7fd423e75b9
bobybi930 <54752946+bobybi930@users.noreply.github.com>
2021-10-01 18:07:43 +0300

    Update GCcontent.py

--- GCcontent.py
+++ GCcontent.py
@@ -1,9 +1,17 @@
 f = open("demofile.txt", "r")
 o = f.readlines()
 dict = dict([])
-for i in range(0, len(o), 3):
-    s = ''.join({o[i + 1], o[i + 2]}).strip()
-    dict[o[i][1:].strip()] = (s.count('G') + s.count('C')) / len(s)
+t = ""
+s = ""
+for i in range(0, len(o)):
+    if o[i][0] == '>':
+        if not i == 0:
+            dict[t] = (s.count('G') + s.count('C')) / len(s)
+        t = o[i][1:].strip()
+        s = ""
+    else:
+        s = ''.join({s, o[i].strip()}).strip()
+dict[t] = (s.count('G') + s.count('C')) / len(s)
 dict = {k: v for k, v in reversed(sorted(dict.items(), key=lambda item: item[1]))}
 print(list(dict.items())[0][0])
 print(list(dict.items())[0][1] * 100)
